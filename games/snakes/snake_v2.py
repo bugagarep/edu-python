e93dfa625abbc02c9642d3bb34e4f3064d54b24d
edu <edu.email@gmail.com>
2026-04-08 23:43:00 +0300

    added snake game as lesson 2

---
+++ games/snakes/snake_v2.py
@@ -0,0 +1,68 @@
+import pygame
+import sys
+import random
+
+# -------------------- settings --------------------
+CELL = 20                # size of cell (pixels)
+GRID_W, GRID_H = 32, 24  # field size in cells (width x height)
+W, H = GRID_W * CELL, GRID_H * CELL
+
+
+BG_COLOR = (18, 18, 18)
+GRID_COLOR = (30, 30, 30)
+SNAKE_HEAD = (80, 200, 120)
+SNAKE_BODY = (60, 160, 100)
+FOOD_COLOR = (220, 70, 70)
+TEXT_COLOR = (230, 230, 230)
+# --------------------------------------------------
+
+def draw_grid(surface):
+    """Draws the grid on the screen"""
+    for x in range(0, W, CELL):
+        pygame.draw.line(surface, GRID_COLOR, (x, 0), (x, H))
+    for y in range(0, H, CELL):
+        pygame.draw.line(surface, GRID_COLOR, (0, y), (W, y))
+
+def draw_cell(surface, position, color):
+    x, y = position
+    rect = pygame.Rect(x * CELL, y * CELL, CELL, CELL)
+    pygame.draw.rect(surface, color, rect, border_radius=4)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((W, H))
+    pygame.display.set_caption("Snake")
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("consolas", 20)
+    big_font = pygame.font.SysFont("consolas", 36)
+
+    while True:
+        # --------------- Events ----------------
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # --------------- Render ----------------
+        screen.fill(BG_COLOR)
+        draw_grid(screen)
+
+        x, y = GRID_W//2, GRID_H//2
+        snake = [(x, y), (x-1, y), (x-2, y)]
+        # i, seg in enumerate
+        # Output:
+        # i -> 0: seg -> 16
+        # i -> 1: seq -> 15
+        # i -> 2: seq -> 15
+        for i, seg in enumerate(snake):
+            draw_cell(screen, seg, SNAKE_HEAD if i == 0 else SNAKE_BODY)
+
+        draw_cell(screen, (3, 5), FOOD_COLOR)
+
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
